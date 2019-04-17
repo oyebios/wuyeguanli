29b1886d9e60c399a7e9523b860b0a485dbb1b0e
lwd <1120647299@qq.com>
2019-04-17 17:24:15 +0800

    ascii

--- app/src/main/java/test/cn/sjz/testproject/base/ARouterPath.java
+++ app/src/main/java/test/cn/sjz/testproject/base/ARouterPath.java
@@ -8,4 +8,5 @@ public class ARouterPath {
     public static final String ACTIVITY_MAIN = "/activity/activity_main";
     public static final String ACTIVITY_LOGIN = "/activity/activity_login";
     public static final String ACTIVITY_FICTION = "/activity/activity_fiction";
+    public static final String ACTIVITY_ASCII = "/activity/activity_ascii";
 }
