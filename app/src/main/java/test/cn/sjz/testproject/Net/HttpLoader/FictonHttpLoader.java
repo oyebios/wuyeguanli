de6d1ad84ac86e8911480caaa991bf963f3e70a2
lwd <1120647299@qq.com>
2019-04-30 22:15:09 +0800

    完成

--- app/src/main/java/test/cn/sjz/testproject/Net/HttpLoader/FictonHttpLoader.java
+++ app/src/main/java/test/cn/sjz/testproject/Net/HttpLoader/FictonHttpLoader.java
@@ -4,11 +4,8 @@ import io.reactivex.Observable;
 import retrofit2.http.GET;
 import test.cn.sjz.testproject.Net.RetrofitFactory;
 import test.cn.sjz.testproject.base.BaseHttpLoader;
-import test.cn.sjz.testproject.fiction.contract.FictionContract;
 
-/**
- * Created by lwd on 2019/4/1.
- */
+
 
 public class FictonHttpLoader extends BaseHttpLoader{
     FictionService service ;
